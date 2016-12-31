d4d8f26d85bb05813c7d0a52e2a2c3af9eed2f3f
Declercq <marc.declercq@laposte.net>
2016-12-31 10:57:20 +0100

    Initialisation des pages Twig et reprise Header
    Création des routes et twig associée aux pages définies dans la
    structure projet.
    Reprise du Header pour utilisation composant bootstrap
    Alignement des icônes de connexion et de synchronisation

--- src/NAO/CoreBundle/Controller/DefaultController.php
+++ src/NAO/CoreBundle/Controller/DefaultController.php
@@ -8,10 +8,62 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
+     * @Route("/observer", name="nao_observer")
+     */
+    public function observerAction() {
+        return $this->render('NAOCoreBundle:Observer:observer.html.twig');
+    }
+
+    /**
+     * @Route("/mes_observations", name="nao_mes_observations")
+     */
+    public function mesObservationsAction() {
+        return $this->render('NAOCoreBundle:Observer:mesObservations.html.twig');
+    }
+
+    /**
+     * @Route("/validations", name="nao_observations_a_valider")
+     */
+    public function validationsAction() {
+        return $this->render('NAOCoreBundle:Observer:observationAValider.html.twig');
+    }
+
+    /**
+     * @Route("/mon_compte", name="nao_mon_compte")
+     */
+    public function monComptesAction() {
+        return $this->render('NAOCoreBundle:Utilisateur:monCompte.html.twig');
+    }
+
+    /**
+     * @Route("/admin", name="nao_gestion_droits")
+     */
+    public function adminAction() {
+        return $this->render('NAOCoreBundle:Utilisateur:gestionDroits.html.twig');
+    }
+
+    /**
+     * @Route("/mentions_legales", name="nao_mentions_legales")
+     */
+    public function mentionsLegalesAction() {
+        return $this->render('NAOCoreBundle:Divers:mentionsLegales.html.twig');
+    }
+
+    /**
+     * @Route("/cgu", name="nao_cgu")
+     */
+    public function cguAction() {
+        return $this->render('NAOCoreBundle:Divers:cgu.html.twig');
+    }
+
+
+    /**
      * @Route("/test_base")
      */
     public function indexAction() // Utilisé seulement pour tester la définition de base.html.twig
     {
         return $this->render('NAOCoreBundle:Default:index.html.twig');
     }
+
+
 }
