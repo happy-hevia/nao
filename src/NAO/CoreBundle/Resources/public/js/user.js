edd1412240cd3f4b3316ae5447991755351ba2e8
happy-hevia <happyhevia@gmail.com>
2017-01-13 17:08:09 +0100

    Création de l'objet currentUserStorage pour gérer l'utilisateur courrant

--- src/NAO/CoreBundle/Resources/public/js/user.js
+++ src/NAO/CoreBundle/Resources/public/js/user.js
@@ -4,12 +4,18 @@
 
 var currentUser;
 
+// Récupère l'utilisateur courant à chaque chargement de page depuis le local storage
+currentUserStorage.getCurrentUser()
+
+
 /* Définition de la classe User */
-function User(nom, prenom, email) {
+function User(nom, prenom, email, pseudo, role) {
     this.valueList=["null", "Visiteur","Naturaliste", "Admin"];
     this.nom = nom;
     this.prenom = prenom;
+    this.pseudo = pseudo;
     this.email = email;
+    this.role = role;
     this.setNull = function () {
         console.log("User=Null");
         currentUser=this.valueList[0];
@@ -30,4 +36,24 @@ function User(nom, prenom, email) {
         currentUser=this.valueList[3];
         updateDOMElementVisibility();
     };
+}
+
+// Gestion du formualaire de connexion en mode hors ligne
+function gestionFormulaireConnexionHorsLigne(){
+    var formulaire = $('#form-login-offline');
+    var select = $('#form-login__user');
+
+    //Je récupère les utilisateurs locaux
+    userStorage.getAll();
+
+    // si il n'y a pas d'utilisateurs locaux, on le notifie à l'internaute
+    if (userStorage.coll == null) {
+        formulaire.replaceWith("<div class='alert alert-warning' role='alert'><strong>OUPS !</strong> Il n'y a d'utilisateur disponible en mode hors ligne, merci de vous connectez à votre compte en ligne avant de pouvoir accéder à votre compte en mode hors ligne</div>")
+
+    //    si il y a des utilisateurs locaux, on les intégrent dans le formulaire
+    } else {
+        for(var utilisateur in userStorage.coll) {
+
+        }
+    }
 }
