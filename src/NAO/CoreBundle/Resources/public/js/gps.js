e09bf8a5388aba27cbdc9f02f4524097985919cf
happy-hevia <happyhevia@gmail.com>
2017-01-19 21:26:40 +0100

    base page Observer

--- src/NAO/CoreBundle/Resources/public/js/gps.js
+++ src/NAO/CoreBundle/Resources/public/js/gps.js
@@ -90,19 +90,14 @@ Localisation.testGPS = function(){
 
 
 Localisation.initListener = function() {
-    Localisation.setIndisponible();
+    Localisation.testAvailable();
     setInterval(Localisation.testGPS,2000);
 };
 
+Localisation.distance = function(lat1, lon1, lat2, lon2) {
+    var p = 0.017453292519943295; // Math.PI / 180
+    var c = Math.cos;
+    var a = 0.5 - c((lat2 - lat1) * p)/2 + c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p))/2;
 
-
-function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
-    var R = 6371; // Radius of the earth in km
-    var dLat = deg2rad(lat2-lat1); // deg2rad below
-    var dLon = deg2rad(lon2-lon1);
-    var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon/2) * Math.sin(dLon/2) ;
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    var d = R * c; // Distance in km
-    return d; }
-
-function deg2rad(deg) { return deg * (Math.PI/180) }
+    return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+}
--- src/NAO/CoreBundle/Resources/public/js/page-observer.js
+++ src/NAO/CoreBundle/Resources/public/js/page-observer.js
@@ -2,6 +2,8 @@
  * Created by marcd on 17/01/2017.
  */
 oiseauStorage.loadAll();
+fillTabObserver();
+
 $("#onglet_espece").click(function() {
     //On récupère le nom de l'espèce dans le span form-observation__espece
     var espece = $("#form-observation__espece").text();
@@ -16,3 +18,32 @@ $("#onglet_espece").click(function() {
     $("#espece-nom__").text(espece);
     $("#espece-description__").html(oiseauStorage.storeData[espece].description);
 });
+
+/**
+ * Rempli le tableau des observations sur la page Observer
+ */
+function fillTabObserver(){
+    observationStorage.getAll();
+    var observations = observationStorage.coll;
+
+    var tableContent;
+    for (var observation in observations) {
+        console.log(gpsCoords);
+        var distance = Localisation.distance(gpsCoords.latitude, gpsCoords.longitude, observation.latitude, observation.longitude);
+        tableContent = tableContent + "<tr ><td data-time='" + observations[observation].date + ">" + observations[observation].oiseauId + " - " + observations[observation].observateur + "</td >" +
+                        "<td >" + observations[observation].latitude + ", " + observations[observation].longitude + "</td >" +
+                        "<td >" + distance + "</td ></tr>";
+
+    }
+
+    $('#emplacement__ligne tbody').append(tableContent);
+}
+
+/**
+ * Permet d'afficher la modal avec les informations de l'espèce quand l'internaute clique sur le nom de l'espèce
+ */
+function affichageInformationObservation(){
+    $('#emplacement__ligne td:first-of-type').click(function(){
+
+    })
+}
